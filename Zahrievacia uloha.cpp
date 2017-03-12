f8a22c0f2bd9fa239f8741244a01d04f81b0bf4c
MilanLinkes <milanlinkes666@gmail.com>
2017-03-12 09:41:19 +0100

    Add files via upload

---
+++ Zahrievacia uloha.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SHORT_INT_BITS 16	
+ 
+short int swap_endianness(short int x){
+	
+	return (x >> 8)|(x << (SHORT_INT_BITS-8));
+}
+
+int main(void){
+	short int cislo = 10;
+	short int x;
+	
+	x = swap_endianness(cislo);
+	printf("%d ", x);
+}
